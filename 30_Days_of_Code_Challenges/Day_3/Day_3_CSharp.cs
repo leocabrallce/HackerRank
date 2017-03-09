0e607ac629d194f54d15057a985b7633a80201d2
Leonardo Cabral Evaristo <leocabrallce@gmail.com>
2017-03-09 20:26:30 -0300

    Create Day_3_CSharp.cs

---
+++ 30_Days_of_Code_Challenges/Day_3/Day_3_CSharp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int N = Convert.ToInt32(Console.ReadLine());
+     
+        if (N % 2 == 1)
+            {
+            Console.WriteLine("Weird");
+        }
+        else if (N % 2 == 0)
+            {
+            if (N <= 20 && N >= 6)
+                {
+                Console.WriteLine("Weird");
+            }
+            else {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
